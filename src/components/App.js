0cd9d101576bae23a097b7fb574ba5e4226bce3e
hadiyajahagirdar <115102788+hadiyajahagirdar@users.noreply.github.com>
2023-05-10 19:28:48 +0530

    submitting code-Wed May 10 2023 19:28:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => {
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <div>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-export default App;
